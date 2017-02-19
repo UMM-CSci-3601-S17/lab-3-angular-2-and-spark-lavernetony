e1c8e1534412b1b0933e4462ac182fdaa445fc5a
Laverne Schrock <schr1230@morris.umn.edu>
2017-02-19 13:21:20 -0600

    Implement method for converting a string to a color

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -3,6 +3,7 @@ import { TodoListService } from "./todo-list.service";
 import { Todo } from "./todo";
 import { FormsModule } from '@angular/forms';
 import { FilterBy } from "../users/filter.pipe";
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Component({
     selector: 'todo-list-component',
@@ -26,6 +27,17 @@ export class TodoListComponent implements OnInit{
             }
         );
     }
+
+    public stringToColorString(input: string): string {
+        let hashCode:any = Md5.hashStr(input);
+
+        let r:string = hashCode.substring(0,2);
+        let g:string = hashCode.substring(2,4);
+        let b:string = hashCode.substring(4,6);
+
+        return "#" + r + g + b;
+    }
+
 }
 
 
